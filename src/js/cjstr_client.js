6cf999c465fb1fb2af7c620cceda1f7ea11a0e9b
Михаил Кормановский <kormanowsky@gmail.com>
2024-07-29 02:26:12 +0300

    remove global cmakelists.txt and promise from c-to-js module

--- src/js/cjstr_client.js
+++ src/js/cjstr_client.js
@@ -1,55 +1,57 @@
-const cjstrFactory = require('./cjstr.js');
+const factory = require('./cjstr.js');
 
-async function main(cjstr) {
-    class CJSStr {
-        constructor(ptr) {
-            this.ptr = ptr;
+const cjstr = factory();
+
+class CJSStr {
+    constructor(ptr) {
+        this.ptr = ptr;
+    }
+
+    get length() {
+        return cjstr._cjstr_length(this.ptr);
+    }
+
+    concat(...strs) {
+        const stackPtr = cjstr.stackSave();
+
+        const ptr = cjstr.stackAlloc(strs.length * cjstr.POINTER_SIZE);
+
+        cjstr.stackRestore(stackPtr);
+
+        let ptrCpy = ptr;
+
+        for (let i = 0; i < strs.length; ++i) {
+            cjstr.setValue(ptrCpy, strs[i].getPtr(), '*');
+
+            ptrCpy += cjstr.POINTER_SIZE;
         }
 
-        get length() {
-            return cjstr._cjstr_length(this.ptr);
-        }
+        return new CJSStr(cjstr._cjstr_concat(this.ptr, ptr, strs.length));
+    }
 
-        concat(...strs) {
-            const stackPtr = cjstr.stackSave();
+    getPtr() {
+        return this.ptr;
+    }
 
-            const ptr = cjstr.stackAlloc(strs.length * cjstr.POINTER_SIZE);
+    toString() {
+        return cjstr.UTF32ToString(cjstr._cjstr_to_wchar_ptr(this.ptr));
+    }
 
-            cjstr.stackRestore(stackPtr);
+    destroy() {
+        cjstr._cjstr_destroy(this.ptr);
+    }
 
-            let ptrCpy = ptr;
+    static fromString(str) {
+        const ptr = cjstr._cjstr_create(str.length);
 
-            for (let i = 0; i < strs.length; ++i) {
-                cjstr.setValue(ptrCpy, strs[i].getPtr(), '*');
+        const string_ptr = cjstr._cjstr_to_wchar_ptr(ptr);
 
-                ptrCpy += cjstr.POINTER_SIZE;
-            }
+        cjstr.stringToUTF32(str, string_ptr, 4 * (str.length + 1));
 
-            return new CJSStr(cjstr._cjstr_concat(this.ptr, ptr, strs.length));
-        }
-
-        getPtr() {
-            return this.ptr;
-        }
-
-        toString() {
-            return cjstr.UTF32ToString(cjstr._cjstr_to_wchar_ptr(this.ptr));
-        }
-
-        destroy() {
-            cjstr._cjstr_destroy(this.ptr);
-        }
-
-        static fromString(str) {
-            const ptr = cjstr._cjstr_create(str.length);
-
-            const string_ptr = cjstr._cjstr_to_wchar_ptr(ptr);
-
-            cjstr.stringToUTF32(str, string_ptr, 4 * (str.length + 1));
-
-            return new CJSStr(ptr);
-        }
+        return new CJSStr(ptr);
     }
 }
 
-cjstrFactory().then(main);
+const s = CJSStr.fromString("test");
+
+console.log(s.toString());
